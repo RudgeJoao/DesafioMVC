0b3a50facb9355aeaae3ca70f50d527511dc75fb
RudgeJoao <113056261+RudgeJoao@users.noreply.github.com>
2023-08-26 14:24:28 -0300

    Criando a model Lutador, controller Lutador e Views

---
+++ Torneio/Torneio/Data/OracleDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Torneio.Models;
+
+namespace Torneio.Data
+{
+    public class OracleDbContext : DbContext
+    {
+        public OracleDbContext (DbContextOptions<OracleDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lutador> Lutador { get; set; } = default!;
+    }
+}
